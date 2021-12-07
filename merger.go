b50c39b8cd311cc2d365fa16f00554e02aa40174
billettc <charles@cbillette.com>
2021-12-07 13:20:46 -0500

    fix typo

--- merger.go
+++ merger.go
@@ -138,7 +138,7 @@ func (m *Merger) launch() (err error) {
 
 		m.bundler.Commit(highestBundleBlockNum)
 
-		zlog.Info("bundle merger and committed", zap.Stringer("bundle", m.bundler), zap.Time("last_merge_one_block_time", m.bundler.LastMergeOneBlockFile().BlockTime))
+		zlog.Info("bundle merged and committed", zap.Stringer("bundle", m.bundler), zap.Time("last_merge_one_block_time", m.bundler.LastMergeOneBlockFile().BlockTime))
 
 		state := &State{ExclusiveHighestBlockLimit: m.bundler.ExclusiveHighestBlockLimit()}
 		zlog.Info("saving state", zap.Stringer("state", state))
