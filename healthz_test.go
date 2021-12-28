9257db28acd98390620b1db8b67d00a7a0c62d07
Francis Roch <francis@manafont.net>
2021-12-28 10:50:45 -0500

    Add TestHealthz_Check()

---
+++ healthz_test.go
@@ -0,0 +1,27 @@
+package merger
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	pbhealth "github.com/streamingfast/pbgo/grpc/health/v1"
+
+	"github.com/streamingfast/merger/bundle"
+)
+
+func TestHealthz_Check(t *testing.T) {
+	ctx := context.Background()
+	bundler := bundle.NewBundler(5, 5)
+	m := NewMerger(bundler, time.Second, "6969", nil, nil, nil, nil, nil, "")
+
+	request := &pbhealth.HealthCheckRequest{}
+	resp, err := m.Check(ctx, request)
+	if err != nil {
+		panic(err)
+	}
+
+	require.Equal(t, resp.Status, pbhealth.HealthCheckResponse_SERVING)
+}
